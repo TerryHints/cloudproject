0f67c1ca12404c502d344ac23714d10cd71009a3
Matthew Wang <matthewwang08@yahoo.com>
2026-04-18 17:08:10 -0400

    uniqueuserids

--- register.php
+++ register.php
@@ -10,14 +10,18 @@ $username = filter_var($_POST['username'], FILTER_SANITIZE_SPECIAL_CHARS);
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
 $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
+//Current workaround for unique ID's... I know php's uniqid isn't industry standard but it'll do
+$userID = uniqid('user_', true); 
+$creationDate = date('c'); 
+
 try {
     error_log("[register.php] putItem starting for username={$username}, email={$email}");
 
     $result = $client->putItem([
         'TableName' => 'Users',
         'Item' => [
-            "UserID" => ["S" => "unique-user-id-123"],
-            "CreationDate" => ["S" => "2026-04-16T23:55:00Z"],
+            "UserID" => ["S" => $userID],
+            "CreationDate" => ["S" => $creationDate],
             'username' => ['S' => $username],
             'password' => ['S' => $password],
             'email'    => ['S' => $email],
